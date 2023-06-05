334804c6f08b9d57135dbf844d30d6eea724a21d
fermelli <luisfernandosalgadomiguez@gmail.com>
2023-06-04 22:07:25 -0400

    soporte para vue2

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
+import Vue from 'vue';
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = new Vue({
+    render: h => h(App)
+});
+
+app.$mount('#app');
