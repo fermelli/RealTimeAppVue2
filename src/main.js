6826671617d7c2bcc53177b8579b737c3edbac12
fermelli <luisfernandosalgadomiguez@gmail.com>
2023-06-04 22:11:03 -0400

    instalacion de BootstrapVue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue';
-import './style.css'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import './style.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 const app = new Vue({
     render: h => h(App)
